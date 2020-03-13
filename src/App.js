069932b2271ff68725ff9712efb79e49a2f8ea88
alemarzano <ajmarzano@gmail.com>
2020-03-13 18:04:00 -0300

    Aprendiendo Props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Bienvenidos a este curso</h2>
-        <h3>Vamos a aprender React</h3>
-        <p>Hola, estoy usando React</p>        
-      </header>
-    </div>
-  );
+// function Hello (props) {
+//   return <h2>{props.title}</h2>
+// }
+
+//const Hello = (props) => <h2>{props.title}</h2>
+
+class Hello extends Component {
+  render() {
+    return <h2>{this.props.title}</h2>
+  }
 }
+
+class Text extends Component {
+  render() {
+    const textoSegunBool = this.props.boolean ? 'Es cierto!' : 'Falso'
+    const mappedNumbers = this.props.arrayOfNumbers.map(n => n*2)
+
+    return (
+      <div>
+        <p>{this.props.text}</p>
+        <p>{this.props.number}</p>
+        <p>{textoSegunBool}</p>
+        <p>{mappedNumbers.join(', ')}</p>
+        <p>{this.props.objectWithInfo.key}</p>
+      </div>
+    )
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="" />
+          <Hello title="Esto es un props" /> 
+          <Text 
+            arrayOfNumbers={[2,3,10]}
+            boolean 
+            objectWithInfo={{key:'First Value', key2: 'otherValue'}}
+            number={2} 
+            text='Texto en string' 
+          />         
+        </header>
+        
+      </div>
+    );
+  }
+}
+
 
 export default App;
