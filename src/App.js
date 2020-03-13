091a8d80db51d95e0dcf19104f9c893ad70fad72
alemarzano <ajmarzano@gmail.com>
2020-03-13 15:28:59 -0300

    Instalación de React

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Bienvenidos a este curso</h2>
+        <h3>Vamos a aprender React</h3>
+        <p>Hola, estoy usando React</p>        
+      </header>
+    </div>
+  );
+}
+
+export default App;
